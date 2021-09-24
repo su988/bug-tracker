011b7f833678f0af40a78e0700094f2a2187c239
Suyash S <suyash988@gmail.com>
2021-09-24 18:32:24 -0400

    auth route-controller import done

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import authRoutes from './src/routes/auth.js';
+
 dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 const { PORT } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ server/src/controllers/auth.js
@@ -0,0 +1,7 @@
+export const signupUser = async (req, res) => {
+  res.send('hello from signup');
+};
+
+export const loginUser = async (req, res) => {
+  res.send('hello from login');
+};
