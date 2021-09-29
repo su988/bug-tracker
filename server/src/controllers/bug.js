fb6492626c41d03fd1b364d2917f03eb526de55c
Suyash S <suyash988@gmail.com>
2021-09-28 21:54:07 -0400

    get all bugs and notes for the selected project

--- server/src/controllers/bug.js
+++ server/src/controllers/bug.js
@@ -1,6 +1,40 @@
 import { prisma } from '../utils/config.js';
 
-export const getBugs = async (req, res) => {};
+export const getBugs = async (req, res) => {
+  const { projectId } = req.params;
+
+  const selectedProject = await prisma.project.findUnique({
+    where: {
+      id: parseInt(projectId),
+    },
+    include: {
+      members: true,
+      bugs: true,
+      bugs: {
+        include: {
+          notes: true,
+        },
+      },
+    },
+  });
+
+  const currentMembers = selectedProject.members.map(
+    (member) => member.memberId
+  );
+
+  const checkIfUserIsMember = currentMembers
+    .map((memberId) => memberId)
+    .includes(req.userId);
+
+  if (!checkIfUserIsMember) {
+    return res.status(401).send({ message: 'Access denied!' });
+  }
+
+  const bugs = {};
+  bugs.bugs = selectedProject.bugs;
+
+  res.send(bugs);
+};
 
 export const createBug = async (req, res) => {};
 
