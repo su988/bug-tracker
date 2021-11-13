9d2ebad642829fb34cfe1ae51cbcb3d0da8dbcf4
Suyash S <suyash988@gmail.com>
2021-11-12 21:52:04 -0500

    client removed

--- client/src/apiUrl.ts
+++
@@ -1,3 +0,0 @@
-const apiUrl = 'http://localhost:3001';
-
-export default apiUrl;
--- client/src/redux/store.ts
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import reducers from './reducers/index.js';
-
-export const store = createStore(reducers, {});
